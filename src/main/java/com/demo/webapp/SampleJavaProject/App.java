1a6c7aa25aa16e5e5db7bb623a81c530f3592a1c
Dhanya <dhanya@120805>
2025-01-15 15:35:01 +0530

    Added

---
+++ src/main/java/com/demo/webapp/SampleJavaProject/App.java
@@ -0,0 +1,13 @@
+package com.demo.webapp.SampleJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
